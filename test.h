390e4a91b99e91bd4cffa6667a93ec6e88083ce2
赵龙 <DreamZL@users.noreply.github.com>
2019-02-22 14:12:07 +0800

    Create test.h

---
+++ test.h
@@ -0,0 +1,7 @@
+#import once
+#include <iostream>
+#include <sstream>
+#include <windows.h>
+#include <string.h>
+#include "hiredis.h"  //要导入Win32_Interop.lib  hiredis.lib库在项目中，可参考https://www.cnblogs.com/dongsheng/p/4011145.html
+using namespace std;
